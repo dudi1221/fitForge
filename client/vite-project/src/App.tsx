1d900030b02cc74494c9307bc476bdf8a475c821
dudi1221 <d4125716@gmail.com>
2024-01-08 09:57:08 +0200

    fredr

---
+++ client/vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import Ruoter from './features/global/router/Router'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Ruoter/>
+    </>
+  )
+}
+
+export default App;
---
+++ client/vite-project/src/features/challenges/pages/allChallenges/AllChallenges.tsx
@@ -0,0 +1,7 @@
+
+
+const AllChallenges = () => {
+
+};
+
+export default AllChallenges;
